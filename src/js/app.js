ab2459d8074dd2c03c3e86e8bae5e8fa19a4dfc4
Tatiana Bazuto <tbazuto@gmail.com>
2025-05-06 19:23:44 +0300

    add

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 // TODO: write code here
 
 // comment this to pass build
+
 const unusedVariable = "variable";
+console.log(unusedVariable);
 
 // for demonstration purpose only
 export default function demo(value) {
